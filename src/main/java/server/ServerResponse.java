0beebe482a9e723a44c2c0bae436f895826e05ef
sltnsrh <lifenjoy8888@gmail.com>
2022-09-11 19:04:06 +0300

    removed redundant methods in ServerResponse

--- src/main/java/server/ServerResponse.java
+++ src/main/java/server/ServerResponse.java
@@ -3,21 +3,13 @@ package server;
 import com.google.gson.JsonElement;
 
 public class ServerResponse {
-    private String status;
-    private String reason;
-    private JsonElement value;
+    private final String status;
+    private final String reason;
+    private final JsonElement value;
 
     public ServerResponse(String status, String reason, JsonElement value) {
         this.status = status;
         this.reason = reason;
         this.value = value;
-    }
-
-    public void setValue(JsonElement value) {
-        this.value = value;
-    }
-
-    public JsonElement getValue() {
-        return value;
     }
 }
