e31e7423ff9f5304b8c771994edc6b18e859453e
sltnsrh <lifenjoy8888@gmail.com>
2022-09-11 19:07:58 +0300

    was split processData in DataGetter

--- src/main/java/server/service/DataGetter.java
+++ src/main/java/server/service/DataGetter.java
@@ -16,30 +16,43 @@ public class DataGetter implements Executable {
             ReadWriteLocker.readLock.lock();
             JsonElement key = requestFromClient.getKey();
             if (key.isJsonPrimitive() && DataStorage.getDataBase().has(key.getAsString())) {
-                String value = DataStorage.getDataBase().get(key.getAsString()).toString();
-                return ResponseProducer.getValueOk(value);
+                return getResponseFromPrimitive(key);
             }
             if (key.isJsonArray()) {
-                JsonArray keys = key.getAsJsonArray();
-                if (keys.size() == 1) {
-                    String value = DataStorage.getDataBase().get(keys.getAsString()).toString();
-                    return ResponseProducer.getValueOk(value);
-                }
-                JsonElement jsonElementToGet = DataStorage.findElement(keys);
-                if (jsonElementToGet == null) {
-                    return ResponseProducer.getNoSuchKey();
-                }
-                String value;
-                if (jsonElementToGet.isJsonObject()) {
-                    value = jsonElementToGet.getAsJsonObject().toString();
-                } else {
-                    value = jsonElementToGet.getAsJsonPrimitive().toString();
-                }
-                return ResponseProducer.getValueOk(value);
+                return getResponseFromJsonArray(key);
             }
         } finally {
             ReadWriteLocker.readLock.unlock();
         }
         return ResponseProducer.getNoSuchKey();
+    }
+
+    private ServerResponse getResponseFromPrimitive(JsonElement key) {
+        String value = DataStorage.getDataBase().get(key.getAsString()).toString();
+        return ResponseProducer.getValueOk(value);
+    }
+
+    private ServerResponse getResponseFromJsonArray(JsonElement key) {
+        JsonArray keys = key.getAsJsonArray();
+        if (keys.size() == 1) {
+            String value = DataStorage.getDataBase().get(keys.getAsString()).toString();
+            return ResponseProducer.getValueOk(value);
+        }
+        JsonElement jsonElementToGet = DataStorage.findElement(keys);
+        if (jsonElementToGet == null) {
+            return ResponseProducer.getNoSuchKey();
+        }
+        String value = getValueFromJsonElement(jsonElementToGet);
+        return ResponseProducer.getValueOk(value);
+    }
+
+    private String getValueFromJsonElement(JsonElement jsonElement) {
+        String value;
+        if (jsonElement.isJsonObject()) {
+            value = jsonElement.getAsJsonObject().toString();
+        } else {
+            value = jsonElement.getAsJsonPrimitive().toString();
+        }
+        return value;
     }
 }
