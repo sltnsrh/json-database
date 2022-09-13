48fb84348f96ddbf1825336f9716c8cb24fdc8de
sltnsrh <lifenjoy8888@gmail.com>
2022-09-13 16:19:14 +0300

    client was simplified

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -17,23 +17,13 @@ public class Client {
                 DataOutputStream output = new DataOutputStream(socket.getOutputStream())
         ) {
             System.out.println("Client started!");
-            //the loop is just for multithreading tests
-            boolean keepSocket = true;
-            while (keepSocket) {
-                String jsonClientRequest = requestToServer.getStringRequest();
-                output.writeUTF(jsonClientRequest);
-                System.out.println("Sent: " + jsonClientRequest);
-                String serverMsg = input.readUTF();
-                System.out.println("Received: " + serverMsg);
-                if (jsonClientRequest.toLowerCase().contains("exit")) {
-                    keepSocket = false;
-                }
-                Thread.sleep(500);
-            }
-        } catch (IOException e) {
-            throw new RuntimeException("Some problem with connection or input/output.", e);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+            String jsonClientRequest = requestToServer.getStringRequest();
+            output.writeUTF(jsonClientRequest);
+            System.out.println("Sent: " + jsonClientRequest);
+            String serverMsg = input.readUTF();
+            System.out.println("Received: " + serverMsg);
+            } catch (IOException ex) {
+            throw new RuntimeException("Can't establish socket connection.", ex);
         }
     }
 }
