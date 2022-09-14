ad413848df18bad097f01f89fdd73c01799c7975
sltnsrh <lifenjoy8888@gmail.com>
2022-09-14 10:05:11 +0300

    client system out comments refactored

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -16,14 +16,14 @@ public class Client {
                 DataInputStream input = new DataInputStream(socket.getInputStream());
                 DataOutputStream output = new DataOutputStream(socket.getOutputStream())
         ) {
-            System.out.println("Client started!");
+            System.out.println("Client: Client started!");
             String jsonClientRequest = requestToServer.getStringRequest();
             output.writeUTF(jsonClientRequest);
-            System.out.println("Sent: " + jsonClientRequest);
+            System.out.println("Client: Sent: " + jsonClientRequest);
             String serverMsg = input.readUTF();
-            System.out.println("Received: " + serverMsg);
+            System.out.println("Client: Received: " + serverMsg);
             } catch (IOException ex) {
-            throw new RuntimeException("Can't establish socket connection.", ex);
+            throw new RuntimeException("Client: Can't establish socket connection.", ex);
         }
     }
 }
